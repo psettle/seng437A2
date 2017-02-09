9e8321d5a132f325ae54c8fea9d7b0d545c73486
Elaurora <joshua.walters@ucalgary.ca>
2017-02-09 11:35:13 -0700

    Changed test files to test case files

--- src/dataUtilitiesTests/TestGetCumulativePercentages.java
+++ src/dataUtilitiesTests/TestGetCumulativePercentages.java
@@ -1,5 +1,14 @@
 package dataUtilitiesTests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestGetCumulativePercentages {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
--- src/dataUtilitiesTests/TestGetRowTotal.java
+++ src/dataUtilitiesTests/TestGetRowTotal.java
@@ -1,5 +1,14 @@
 package dataUtilitiesTests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestGetRowTotal {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
