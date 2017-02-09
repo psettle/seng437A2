9acaf934f3b9e2ea9176c8d7377b83eb31d2aa58
Eric Matteucci <eric.matteucci@ucalgary.ca>
2017-02-09 11:30:23 -0700

    Added range test cases

--- src/rangeTests/TestGetLowerBound.java
+++ src/rangeTests/TestGetLowerBound.java
@@ -1,5 +1,14 @@
 package rangeTests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestGetLowerBound {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
--- src/rangeTests/TestGetUpperBound.java
+++ src/rangeTests/TestGetUpperBound.java
@@ -1,5 +1,14 @@
 package rangeTests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestGetUpperBound {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
