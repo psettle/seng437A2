03f983aa04b4286894136f75a02759b2f698e9aa
psettle <patricksettle@hotmail.com>
2017-02-09 11:01:04 -0700

    Fixed conflicts

--- src/DataUtilitiesTest.java
+++ src/DataUtilitiesTest.java
@@ -4,23 +4,4 @@ import org.junit.Test;
 
 public class DataUtilitiesTest {
 
-
-	@Test
-<<<<<<< HEAD:src/TestTest.java
-	public void combineTest() {
-		
-		Range r = new Range(6, 9);
-		
-		Range.shift(r, 0);
-		
-		fail(r.toString());
-		
-		//assertEquals("Testing " + new Range(4, 6), r);
-
-=======
-	public void test() {
-		fail("Not yet implemented");
->>>>>>> 2d52ba2a57196c4ceb89d7e190813b0caf189f51:src/DataUtilitiesTest.java
-	}
-
 }
