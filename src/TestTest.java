fcc5b3156307e0ce9eebf748ff363676f8936190
psettle <patricksettle@hotmail.com>
2017-02-09 09:18:20 -0700

    Added libs

---
+++ src/TestTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.jfree.data.Range;
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		new Range(-1.0, 1.0);
+		fail("Not yet implemented");
+	}
+
+}
