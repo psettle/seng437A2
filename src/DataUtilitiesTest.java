2d52ba2a57196c4ceb89d7e190813b0caf189f51
psettle <patricksettle@hotmail.com>
2017-02-09 10:22:44 -0700

    Added test case stubs

--- src/DataUtilitiesTest.java
+++ src/DataUtilitiesTest.java
@@ -1,13 +1,11 @@
 import static org.junit.Assert.*;
 
-import org.jfree.data.Range;
 import org.junit.Test;
 
-public class TestTest {
+public class DataUtilitiesTest {
 
 	@Test
 	public void test() {
-		new Range(-1.0, 1.0);
 		fail("Not yet implemented");
 	}
 
