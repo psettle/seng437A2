57b7d86854e99487f21cf4158acf276b88bb0f51
Eric Matteucci <eric.matteucci@ucalgary.ca>
2017-02-09 10:53:59 -0700

    committing changes so I may pull

--- src/TestTest.java
+++ src/TestTest.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 
 public class TestTest {
 
+
 	@Test
-	public void test() {
-		new Range(-1.0, 1.0);
-		fail("Not yet implemented");
+	public void combineTest() {
+		
+		Range r = new Range(6, 9);
+		
+		Range.shift(r, 0);
+		
+		fail(r.toString());
+		
+		//assertEquals("Testing " + new Range(4, 6), r);
+
 	}
 
 }
