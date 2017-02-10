8154146cf64e44b4b2922520840cfd9ea15e4622
Eric Matteucci <eric.matteucci@ucalgary.ca>
2017-02-09 22:36:10 -0700

    added more tests for the row and column things

--- src/dataUtilitiesTests/TestGetColumnTotal.java
+++ src/dataUtilitiesTests/TestGetColumnTotal.java
@@ -2,6 +2,8 @@ package dataUtilitiesTests;
 
 import static org.junit.Assert.*;
 
+import java.security.InvalidParameterException;
+
 import org.jfree.data.DataUtilities;
 import org.jfree.data.Values2D;
 import org.jmock.*;
@@ -22,26 +24,30 @@ public class TestGetColumnTotal {
 	}
 	
 	@Test
-	public void testSummingWithInts() {
+	public void testColumnTotalWithInts() {
 		
 		mockingContext.checking(new Expectations() {
 			{
 			one(values).getRowCount();
-			will(returnValue(2));
+			will(returnValue(4));
 			one(values).getValue(0, 1);
 			will(returnValue(3));
 			one(values).getValue(1, 1);
 			will(returnValue(4));
+			one(values).getValue(2, 1);
+			will(returnValue(0));
+			one(values).getValue(3, 1);
+			will(returnValue(63));
 			}
 		});
 		
 		double result = DataUtilities.calculateColumnTotal(values, 1);
 		
-		assertEquals(7, result, 0.0000001d);
+		assertEquals("Testing a column total with a column of integers", 70, result, 0.0000001d);
 	}
 	
 	@Test
-	public void testSummingWithDoubles() {
+	public void testColumnTotalWithDoubles() {
 		
 		mockingContext.checking(new Expectations() {
 			{
@@ -58,8 +64,67 @@ public class TestGetColumnTotal {
 		
 		double result = DataUtilities.calculateColumnTotal(values, 1);
 		
-		assertEquals(13.2, result, 0.0000001d);
+		assertEquals("Testing a column total with a column of doubles", 13.2, result, 0.0000001d);
 	}
+	
+	@Test
+	public void testColumnTotalWithNoRows() {
+		
+		mockingContext.checking(new Expectations() {
+			{
+			one(values).getRowCount();
+			will(returnValue(0));
+			}
+		});
+		
+		double result = DataUtilities.calculateColumnTotal(values, 1);
+		
+		assertEquals("Testing a column total without any rows.", 0, result, 0);
+	}
+	
+
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void testNegativeRowIndex() {
+		
+		mockingContext.checking(new Expectations() {
+			{
+				one(values).getRowCount();
+				will(returnValue(3));
+				one(values).getValue(0, -1);
+				will(throwException(new IndexOutOfBoundsException()));
+				one(values).getValue(1, -1);
+				will(throwException(new IndexOutOfBoundsException()));
+				one(values).getValue(2, -1);
+				will(throwException(new IndexOutOfBoundsException()));
+			}
+		});
+		
+		double result = DataUtilities.calculateColumnTotal(values, -1);
+		
+	}
+	
+	@Test (expected = IndexOutOfBoundsException.class)
+	public void testIndexOutOfBounds() {
+		
+		mockingContext.checking(new Expectations() {
+			{
+				one(values).getRowCount();
+				will(returnValue(3));
+				one(values).getValue(0, 5);
+				will(throwException(new IndexOutOfBoundsException()));
+				one(values).getValue(1, 5);
+				will(throwException(new IndexOutOfBoundsException()));
+				one(values).getValue(2, 5);
+				will(throwException(new IndexOutOfBoundsException()));
+			}
+		});
+		
+		double result = DataUtilities.calculateColumnTotal(values, 5);
+		
+	}
+	
+	
+	
 
 
 }
--- src/dataUtilitiesTests/TestGetRowTotal.java
+++ src/dataUtilitiesTests/TestGetRowTotal.java
@@ -2,13 +2,129 @@ package dataUtilitiesTests;
 
 import static org.junit.Assert.*;
 
+import java.security.InvalidParameterException;
+
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.Values2D;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestGetRowTotal {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+
+	private Mockery mockingContext;
+	private Values2D values;
+	
+	
+	@Before
+	public void setup() {
+		
+		mockingContext = new Mockery();
+		values = mockingContext.mock(Values2D.class);
+		
 	}
+	
+	@Test
+	public void testRowTotalWithInts() {
+		
+		mockingContext.checking(new Expectations() {
+			{
+				one(values).getColumnCount();
+				will(returnValue(4));
+				one(values).getValue(1, 0);
+				will(returnValue(3));
+				one(values).getValue(1, 1);
+				will(returnValue(0));
+				one(values).getValue(1, 2);
+				will(returnValue(9));
+				one(values).getValue(1, 3);
+				will(returnValue(4));
+			}
+		});
+		
+		double result = DataUtilities.calculateRowTotal(values, 1);
+		
+		assertEquals("Testing row total with a row on integers", 16, result, 0.0000001d);
+	}
+	
+	@Test
+	public void testRowTotalWithDoubles() {
+		
+		mockingContext.checking(new Expectations() {
+			{
+				one(values).getColumnCount();
+				will(returnValue(3));
+				one(values).getValue(1, 0);
+				will(returnValue(3.695));
+				one(values).getValue(1, 1);
+				will(returnValue(0));
+				one(values).getValue(1, 2);
+				will(returnValue(8.888));
+			}
+		});
+		
+		double result = DataUtilities.calculateRowTotal(values, 1);
+		
+		assertEquals("Testing row total with row of doubles", 12.583, result, 0.0000001d);
+	}
+	
+	@Test
+	public void testRowTotalWithNoColumns() {
+		
+		mockingContext.checking(new Expectations() {
+			{
+				one(values).getColumnCount();
+				will(returnValue(0));
+			}
+		});
+		
+		double result = DataUtilities.calculateRowTotal(values, 0);
+		
+		assertEquals("Testing row total with no columns", 0, result, 0.0000001d);
+	}
+	
+	@Test
+	public void testInvalidColumnNumber() {
+		
+		mockingContext.checking(new Expectations() {
+			{
+				one(values).getColumnCount();
+				will(returnValue(3));
+				one(values).getValue(1, 0);
+				will(returnValue(3.695));
+				one(values).getValue(1, 1);
+				will(returnValue(0));
+				one(values).getValue(1, 2);
+				will(returnValue(8.888));
+			}
+		});
+		
+		double result = DataUtilities.calculateRowTotal(values, 1);
+		
+		assertEquals("Testing row total with row of doubles", 12.583, result, 0.0000001d);
+	}
+	
+	@Test
+	public void testNegativeColumnIndex() {
+		
+		mockingContext.checking(new Expectations() {
+			{
+				one(values).getColumnCount();
+				will(returnValue(3));
+				one(values).getValue(0, -1);
+				will(throwException(new IndexOutOfBoundsException()));
+			}
+		});
+		
+		double result = DataUtilities.calculateRowTotal(values, -1);
+		
+	}
+	
+	
+
+
 
 }
